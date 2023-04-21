c55dde7922eba6d9d428697a65e36e8e093ae387
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-04-21 12:07:01 +0000

    playing around with gpt access

--- ChatGPT-Measure/Program.cs
+++ ChatGPT-Measure/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using System.IO;
+
+
+string data = File.ReadAllText("Documentation.txt");
+
+string initialMessage = "Please provide a summary of the following: \n\n Text: \n";
+string endMessage = "\nSummary: ";
+
+Console.WriteLine(initialMessage + data + endMessage);
+
+string endpoint = "https://dm-openai-test-env.openai.azure.com/";
+
+OpenAIClient client = new OpenAIClient(new Uri(endpoint), new DefaultAzureCredential());
+var options = new CompletionsOptions();
+options.Prompts.Add(initialMessage + data + endMessage);
+
+var response = await client.GetCompletionsStreamingAsync(
+    deploymentOrModelName: "gpt4-dm",
+    options
+);
+
+using StreamingCompletions streamingCompletions = response.Value;
+
+await foreach(StreamingChoice choice in streamingCompletions.GetChoicesStreaming())
+{
+    await foreach(var text in choice.GetTextStreaming()){
+        Console.Write(text);
+    }
+    Console.WriteLine();
+}
